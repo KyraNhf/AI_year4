083201eeabe0474c83e6c5ceaf002685a98fed09
KyraNoordhof <Kyra.nhf@gmail.com>
2024-11-25 16:18:18 +0100

    opdr1 verbeterd

--- week 1/river-crossing/river-crossing.py
+++ week 1/river-crossing/river-crossing.py
@@ -7,64 +7,39 @@ wolf = 'W'
 
 state = ({farmer, goat, cole, wolf}, set())
 
-
 def find_all_paths(node, path=[]):
     path = path + [node]
-
-    # check for endstate
-    if is_goal_state(node) :
+    if {wolf, goat, farmer, cole}.issubset(node[1]):
         return [path]
-
-    # check for invalid states
     if is_invalid_state(node) :
         return [path]
-
     paths = []
-
     for child in next_states(node):
         if child not in path:
             new_paths = find_all_paths(child, path)
             for new_path in new_paths:
                 paths.append(new_path)
-
     return paths
 
-def move_right(node, item):
-    left = node[0]
-    left.remove(item)
-    left.remove(farmer)
-
-    right = node[1]
-    right.add(item)
-    right.add(farmer)
-
+def move_items(node, direction, item):
+    left, right = node
+    if direction == left:
+        if item is not farmer:
+            right.remove(item)
+            left.add(item)
+        right.remove(farmer)
+        left.add(farmer)
+    elif direction == right:
+        left.remove(item)
+        left.remove(farmer)
+        right.add(item)
+        right.add(farmer)
     next_node = (left, right)
     return next_node
-
-def move_left(node, item=None):
-    right = node[1]
-    left = node[0]
-
-    if item is not farmer:
-        right.remove(item)
-        left.add(item)
-
-    right.remove(farmer)
-    left.add(farmer)
-
-    next_node = (left, right)
-    return next_node
-
-def is_goal_state(node):
-    right = node[1]
-    if {wolf, goat, farmer, cole}.issubset(right):
-        return True
-    return False
 
 def is_invalid_state(node):
     left = node[0]
     right = node[1]
-
     if ({wolf, goat, cole}.issubset(left) and {farmer}.issubset(right)) or ({wolf, goat, cole}.issubset(right) and {farmer}.issubset(left)):
         return True
     elif ({wolf, goat}.issubset(left) and {farmer, cole}.issubset(right)) or ({wolf, goat}.issubset(right) and {farmer, cole}.issubset(left)):
@@ -78,20 +53,17 @@ def next_states(node):
     states = []
     left = node[0]
     right = node[1]
-
     if {farmer}.issubset(left) :
         for item in left:
             if item is not farmer:
                 n = deepcopy(node)
-                move = move_right(n, item)
+                move = move_items(n, right, item=item)
                 states.append(move)
-
     elif {farmer}.issubset(right) :
         for item in right:
             n = deepcopy(node)
-            move = move_left(n, item)
+            move = move_items(n, left, item=item)
             states.append(move)
-
     return states
 
 def print_solution(paths):

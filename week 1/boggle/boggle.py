f4d52cb4afa7541d74a0dc2672bc6b5ddd95e7dc
Matthias <matthiasnijman@hotmail.com>
2024-11-25 14:00:16 +0100

    Boggle start

---
+++ week 1/boggle/boggle.py
@@ -0,0 +1,11 @@
+prefixes = ...
+
+test_bord = [['P','I','E','T'],
+             ['G','A','A','T'],
+             ['A','T','M','S'],
+             ['H','U','I','S']]
+
+def find_all_paths(board):
+    for row in board:
+        for col in row:
+            ...

74adfba7d6c90e6f89ad1f672102d8747d5ca90a
KyraNhf <k.noordhof@st.hanze.nl>
2025-01-21 11:56:27 +0100

    opdr 2 c

--- week 5/start_generator.py
+++ week 5/start_generator.py
@@ -22,6 +22,17 @@ if state in d:
 # generate some sentences
 
 # train and print model for n=3
+text_model_three = markovify.Text(text, state_size=3)
+d_three = text_model_three.chain.model
+# pprint(d)
+
+state_three = ('Japi', 'wist', 'wel')
+if state_three in d_three:
+    next_words_three = d_three[state_three]
+    pprint(next_words_three)
+    total_freq = sum(next_words_three.values())
+    prob = {word: freq/total_freq for word, freq in next_words_three.items()}
+    print(prob)
 
 # generate sentences n=3
 

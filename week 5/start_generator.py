d99f25c0ec31ca6da09fb5b530a02ee8c845bb5e
KyraNhf <k.noordhof@st.hanze.nl>
2025-01-21 11:52:24 +0100

    opdr 2 b af

--- week 5/start_generator.py
+++ week 5/start_generator.py
@@ -9,7 +9,16 @@ text = f.read()
 # train and print model for n=2
 text_model = markovify.Text(text, state_size=2)
 d = text_model.chain.model
-pprint(d)
+# pprint(d)
+
+state = ('Gare', 'du')
+
+if state in d:
+    next_words = d[state]
+    pprint(next_words)
+    total_freq = sum(next_words.values())
+    prob = {word: freq/total_freq for word, freq in next_words.items()}
+    print(prob)
 # generate some sentences
 
 # train and print model for n=3
